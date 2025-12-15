644dddd00b4bed31dde3ebc906ebbc0dd428f2c5
Damien Lebas <66518969+Dam77@users.noreply.github.com>
2025-12-15 01:10:18 +0100

    Delete DG directory

--- DG/utils.py
+++
@@ -1,4 +0,0 @@
-def ask_parameter(iterations: int, tx: float) -> tuple[int, float]:
-    iterations = int(input(f"Enter the number of iterations (default {iterations}): "))
-    tx = float(input(f"Enter the learning rate"))
-    return iterations, tx
